0d7a1e533748cee29c644e69215eb4c69a1d5dc7
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-10-01 12:18:17 +0000

    :pencil2: made changes to the spacing of the lines

--- utils/env_loader.js
+++ utils/env_loader.js
@@ -4,17 +4,17 @@ import { existsSync, readFileSync } from 'fs';
  * Represents an environment loader.
  */
 const envLoader = () => {
-    const env = process.env.NODE_ENV || 'dev';
-    const path =  env.includes('test') || env.includes('cover') ? '.env.test' : '.env';
+  const env = process.env.NODE_ENV || 'dev';
+  const path = env.includes('test') || env.includes('cover') ? '.env.test' : '.env';
 
-    if (!existsSync(path)) {
-        const data = readFileSync(path, 'utf8').trim().split('\n');
+  if (!existsSync(path)) {
+    const data = readFileSync(path, 'utf8').trim().split('\n');
 
-        for (const line of data) {
-            const [key, value] = line.split('=');
-            process.env[key] = value;
-        }
+    for (const line of data) {
+      const [key, value] = line.split('=');
+      process.env[key] = value;
     }
+  }
 };
 
 export default envLoader;
